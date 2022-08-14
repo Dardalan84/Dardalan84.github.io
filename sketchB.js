f202f368ce4df153e0f4b932aadcd20ef6d7cd75
Daryoosh Ardalan <99105969+Dardalan84@users.noreply.github.com>
2022-08-14 16:46:47 -0600

    Add files via upload

--- sketchB.js
+++ sketchB.js
@@ -2,78 +2,67 @@ let o = 300;
 let p = 300;
 
 function setup() {
-    let canvas = createCanvas(600, 600);
+    let canvas = createCanvas(500, 500);
     canvas.parent('sketch');
 
-
-    background(240,0);
-    
-fill(255);
-   
 }
 
-function draw(){
+function draw() {
     noStroke();
-    fill(mouseX,0,mouseY);
-ellipse(mouseX,mouseY, 80,80);
+    fill(mouseX, 0, mouseY);
+    ellipse(mouseX, mouseY, random(40, 50), random(40, 50));
+    noFill();
+    strokeWeight(160);
+    stroke(10, 0, 86, 12);
+
+    ellipse(width / 2, height / 2, 600, 600);
 
 
-noFill();
-strokeWeight(180);
-stroke(10,0,86,12);
 
-ellipse(width/2,height/2,700,700);
-// strokeWeight(60);
-// stroke(10,0,116,10);
+    textSize(40);
+    noFill();
+    strokeWeight(3);
+    stroke(255);
+    //rect(150, 36, 300, 40);
+    strokeWeight(0);
+    fill(255);
+   // text("Recent Projects", 160, 70);
 
-// point(o,p);
-
-// let randNum = floor(random(4));
-//    if(randNum == 0){
-//        o=o+3;
-//    } else if(randNum == 1){
-//     o=o-3;
-// } else if(randNum == 2){
-//     p=p+3;
-// } else if(randNum == 3){
-//     p=p-3;
-// };
-
-
-textSize(40);
-noFill();
-strokeWeight(3);
-stroke(255);
-rect(150,36,300,40);
-strokeWeight(0);
-fill(255);
-    text("Recent Projects", 160, 70);
-    textSize(28);
-    text("Puzzle Arcade", 100,160);
 
 }
 
 function mousePressed() {
     noStroke();
-    fill(10,0,116);
+    fill(10, 0, 116);
+    ellipse(random(width ), random(height ), 800, 800);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
+    ellipse(random(width ), random(height ), 80, 80);
 
-     ellipse(random(width-200), random(height-200), 80, 80);
-     ellipse(random(width-200), random(height-200), 80, 80);
-     ellipse(random(width-200), random(height-200), 80, 80);
-  
+
 }
 
 class Hole {
     constructor() {
-        this.x=random(width);
-        this.y=random(height);
-        this.size=random(20,60);
-        this.col=color(240);
+        this.x = random(width);
+        this.y = random(height);
+        this.size = random(20, 60);
+        this.col = color(240);
     }
 
     show() {
         fill(this.col);
-        ellipse(this.x,this.y, this.size, this.size);
+        ellipse(this.x, this.y, this.size, this.size);
     }
 }
 
